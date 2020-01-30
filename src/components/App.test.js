11cc5259e3f743ecd9be4c2a28249c19cb3876fd
Jodi N Kansagor <joedvice@gmail.com>
2020-01-29 16:08:55 -0800

    styling done

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-describe('App comp
-onent', () => {
+describe('App component', () => {
   it('renders App', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
